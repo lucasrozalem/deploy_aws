0e65042fb1a76bccdace448910df0dc31fd6c93d
Lucas Rozalem <lrozalem@gmail.com>
2020-11-13 16:01:50 -0300

    first commit

--- index.js
+++ index.js
@@ -6,14 +6,8 @@ app.get('/',(request, response)=>{
   return response.json({message: 'Server is up'})
 })
 
-app.get('/atualizou',(request, response)=>{
-  return response.json({message: 'Atualizou mesmo!'})
-})
-
-app.get('/teste',(request, response)=>{
-  const { name, date } = request.body;
-
-  return response.json({name, date})
+app.get('/test',(request, response)=>{
+  return response.json({message: 'Test successfully!'})
 })
 
 app.listen(3333);
